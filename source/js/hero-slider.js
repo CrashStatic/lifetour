d1f1a273a1643cb9f19a6e5a84767cec1cc510a5
CrashStatic <thecrashstatic@gmail.com>
2024-08-10 15:46:50 +0300

    Достраивает слайдер в блоке hero

--- source/js/hero-slider.js
+++ source/js/hero-slider.js
@@ -1,6 +1,6 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 import { Pagination } from 'swiper/modules';
-import 'swiper/css';
+import 'swiper/css/bundle';
 import 'swiper/css/pagination';
 
 const initSwiperHero = () => {
